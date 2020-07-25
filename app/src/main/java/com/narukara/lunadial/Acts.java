342425ff9f574edc7344054094d54750c39b686d
Narukara <hurui317@gmail.com>
2020-07-25 11:43:55 +0800

    init

---
+++ app/src/main/java/com/narukara/lunadial/Acts.java
@@ -0,0 +1,19 @@
+package com.narukara.lunadial;
+
+public abstract class Acts {
+    public final static int SLEEP = 0;
+    public final static int EAT = 1;
+    public final static int CLEAN = 2;
+    public final static int STUDY = 3;
+    public final static int FUN = 4;
+    public final static int OTHER = 5;
+
+    private final static String[] ActName = {"睡眠","就餐","卫生","学习","娱乐","其他"};
+
+    public static String getActName(int id){
+        if(id == -1){
+            return "ID error";
+        }
+        return ActName[id];
+    }
+}
