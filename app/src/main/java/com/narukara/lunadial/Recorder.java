8916e18bb30e0c361c7662688591b9a80ec3758f
Narukara <narukara17@gmail.com>
2020-07-27 00:24:04 +0800

    init xls file

--- app/src/main/java/com/narukara/lunadial/Recorder.java
+++ app/src/main/java/com/narukara/lunadial/Recorder.java
@@ -1,6 +1,11 @@
 package com.narukara.lunadial;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -8,24 +13,47 @@ import java.util.TimeZone;
 public abstract class Recorder {
 
     public static void commit(int id, long time) throws IOException {
-        File target = new File(Pen.fileDir, getDate());
-        if (!target.exists()) {
-            target.createNewFile();
-            Pen.write(target, String.valueOf(id), String.valueOf(time));
-        } else {
-            String already = Pen.read(target, String.valueOf(id));
-            if (already == null) {
-                Pen.write(target, String.valueOf(id), String.valueOf(time));
-            } else {
-                Pen.write(target, String.valueOf(id), String.valueOf(time + Long.parseLong(already)));
+        File xls = new File(Pen.fileDir, getYear() + ".xls");
+        if (!xls.exists()) {
+            initXLS(xls);
+        }
+    }
+
+    public static void initXLS(File file) throws IOException {
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+        HSSFSheet[] hssfSheet = new HSSFSheet[12];
+        for (int i = 1; i <= 12; i++) {
+            hssfSheet[i - 1] = hssfWorkbook.createSheet(String.valueOf(i));
+            HSSFRow hssfRow1 = hssfSheet[i - 1].createRow(0);
+            for (int j = 1; j <= 31; j++) {
+                hssfRow1.createCell(j).setCellValue(j);
+            }
+            for (int j = 0; j < 6; j++) {
+                hssfSheet[i - 1].createRow(j + 1).createCell(0).setCellValue(Acts.getActName(j));
             }
         }
+        hssfWorkbook.write(file);
+        hssfWorkbook.close();
+    }
 
+//    private static String readXLS(File file, int row, int cell) {
+//    }
+
+    private static String getYear() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getDefault());
+        return String.valueOf(calendar.get(Calendar.YEAR));
+    }
+
+    private static String getMonth() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getDefault());
+        return String.valueOf(calendar.get(Calendar.MONTH));
     }
 
     private static String getDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeZone(TimeZone.getDefault());
-        return calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DATE);
+        return String.valueOf(calendar.get(Calendar.DATE));
     }
 }
